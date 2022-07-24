0f07d889ff329e44de727ac01c4085e3eb3d8750
dev.taeyeong <dev.taeyeong@gmail.com>
2022-07-25 00:03:33 +0900

    modify: deploy.sh desc: 구동중인 pid 확인하는 부분 grep jar에서 java로 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ cp $REPOSITORY/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl webservice | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
