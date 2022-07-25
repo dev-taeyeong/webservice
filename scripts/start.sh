80b61471373b387397912ece8bd9f32fd9fe005d
dev.taeyeong <dev.taeyeong@gmail.com>
2022-07-25 23:59:50 +0900

    무중단 배포를 위한 script 추가

---
+++ scripts/start.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+REPOSITORY=/home/ec2-user/app/step3
+PROJECT_NAME=webservice
+
+echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/*.jar $REPOSITORY"
+
+cp $REPOSITORY/*.jar $REPOSITORY/
+
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+IDLE_PROFILE=$(find_idel_profile)
+
+echo "> $JAR_NAME를 profile=$IDLE_PROFILE로 실행합니다."
+nohup java -jar \
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-ouath.properties,/home/ec2-user/app/application-real-db.propertiex \
+  -Dspring.profiles.active=$IDLE_PROFILE \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
