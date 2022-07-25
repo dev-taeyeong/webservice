87e685c85403e735d29a8afbb548c4d953145a0c
dev.taeyeong <dev.taeyeong@gmail.com>
2022-07-26 00:11:01 +0900

    test

--- scripts/health.sh
+++ scripts/health.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ABSPATH=$(readlink 0f $0)
+ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 source ${ABSDIR}/switch.sh
@@ -19,12 +19,12 @@ do
 
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
-    echo "> Health check 성공"
-    switch_proxy
-    break
+      echo "> Health check 성공"
+      switch_proxy
+      break
   else
-    echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-    echo "> Health check: ${RESPONSE}"
+      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+      echo "> Health check: ${RESPONSE}"
   fi
 
   if [ ${RETRY_COUNT} -eq 10 ]
